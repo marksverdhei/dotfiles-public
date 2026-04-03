4ca080e95d1462c65c266d8c06cb19eb7a2b5750
marksverdhei <marksverdhei@hotmail.com>
2026-04-03 11:57:26 +0200

    Add var UNLEASH_DIR

--- bash/vars/common.sh
+++ bash/vars/common.sh
@@ -6,8 +6,11 @@ if [[ -z "$LANG" ]]; then
   export LANG="en_US.UTF-8"
 fi
 
-export COMMIT_MSG_MODEL='qwen3.5-9b'
-export HAT_MODEL='qwen3.5-9b'
+
+export LLM='qwen3.5-27b'
+export VLM="$LLM"
+export COMMIT_MSG_MODEL="$LLM"
+export HAT_MODEL="$LLM"
 
 export HF_HOME="$HOME/Models/huggingface"
 
@@ -17,3 +20,4 @@ export MODELS="$HOME/Models"
 export GGUFS="$HOME/Models/llama.cpp"
 
 unset CLAUDE_CONFIG_DIR
+export UNLEASH_DIR='/home/me/ht/unleash'
